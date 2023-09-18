a4126e2cea04e8d7d00c42e9810dc7995b00669e
Jose Guambo <jlguambo@gmail.com>
2023-09-17 23:46:59 -0500

    fix: Add CI/CD not working to Cloudfare.

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,11 @@
 export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
+  serverConditions: ['workerd', 'worker', 'browser'],
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
